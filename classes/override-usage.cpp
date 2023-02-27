b822decb60c538feaa9b11fda55bf855a5f66a0c
TMTLJH3 <eugene.shubov@cymotive.com>
2023-02-27 05:23:15 -0800

    Add override usage example

---
+++ classes/override-usage.cpp
@@ -0,0 +1,35 @@
+// In a member function declaration or definition, override specifier ensures that the function is virtual 
+// and is overriding a virtual function from a base class. 
+// The program is ill-formed (a compile-time error is generated) if this is not true.
+
+// Which lines produce compilation errors?
+
+#include <iostream>
+ 
+struct A
+{
+    virtual void foo();
+    void bar();
+    virtual ~A();
+};
+ 
+void A::foo() { std::cout << "A::foo();\n"; }
+A::~A() { std::cout << "A::~A();\n"; }
+ 
+struct B : A
+{
+    void foo() const override;
+    void foo() override;
+    void bar() override;
+    ~B() override;
+};
+ 
+void B::foo() { std::cout << "B::foo();\n"; }
+B::~B() { std::cout << "B::~B();\n"; }
+ 
+int main()
+{
+    B b;
+    b.foo();
+    return 0;
+}
